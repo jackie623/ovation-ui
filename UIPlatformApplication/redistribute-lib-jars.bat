c8e80378e305b3a3d84992c223263ba752816962
Barry Wark <barrywark@gmail.com>
2012-07-20 11:12:15 -0400

    Update windows redistribute-lib-jars

--- UIPlatformApplication/redistribute-lib-jars.bat
+++ UIPlatformApplication/redistribute-lib-jars.bat
@@ -1,11 +1,11 @@
 set basedir=%~dp0
 
-IF exist %basedir%\build\cluster\modules\ext ( cp %basedir%\lib\ovation.jar %basedir%\build\cluster\modules\ext )
+if exist %basedir%\build\cluster\modules\ext ( copy /Y %basedir%\lib\ovation.jar %basedir%\build\cluster\modules\ext )
 
-cp %basedir%\lib\ovation.jar %basedir%\Browser\release\modules\ext
-cp %basedir%\lib\ovation.jar %basedir%\DBConnectionProvider\release\modules\ext
-cp %basedir%\lib\ovation.jar %basedir%\OvationAPI\release\modules\ext
+copy /Y %basedir%\lib\ovation.jar %basedir%\Browser\release\modules\ext
+copy /Y %basedir%\lib\ovation.jar %basedir%\DBConnectionProvider\release\modules\ext
+copy /Y %basedir%\lib\ovation.jar %basedir%\OvationAPI\release\modules\ext
 
-cp %basedir%\lib\test-manager.jar %basedir%\TestManager\release\modules\ext
+copy /Y %basedir%\lib\test-manager.jar %basedir%\TestManager\release\modules\ext
 
-cp %basedir%\lib\interfaces.jar %basedir%\GlobalInterfaceLibrary\release\modules\ext\
+copy /Y %basedir%\lib\interfaces.jar %basedir%\GlobalInterfaceLibrary\release\modules\ext\

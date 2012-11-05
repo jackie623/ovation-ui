369996af08e48e6dbc0d87590a4863d7c0c52287
Jackie <jackie@physionconsulting.com>
2012-11-05 10:03:23 -0500

    selection details test manager cant create the test database, but other tests work fine

--- ovation-ui/SelectionDetails/test/unit/src/us/physion/ovation/detailviews/SelectionViewTestManager.java
+++ ovation-ui/SelectionDetails/test/unit/src/us/physion/ovation/detailviews/SelectionViewTestManager.java
@@ -39,10 +39,10 @@ public class SelectionViewTestManager extends TestManager {
                 return pwd;
             }
         });
-        if (pwd == null)
+        /*if (pwd == null)
         {
             pwd = "/Users/huecotanks/Ovation/ui/";
-        }
+        }*/
         String path = pwd + "data" + File.separator
                 + "selectionDetails" + File.separator
                 + "selection-details.connection";

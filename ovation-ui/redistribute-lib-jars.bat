7faf03552d7deb1ffa57a6d5a3aeadb237627f52
Barry Wark <barrywark@gmail.com>
2013-01-16 11:42:53 -0500

    Updated windows redistribute jars to create destination dirs

--- ovation-ui/redistribute-lib-jars.bat
+++ ovation-ui/redistribute-lib-jars.bat
@@ -2,10 +2,17 @@ set basedir=%~dp0
 
 if exist %basedir%\build\cluster\modules\ext ( copy /Y %basedir%\lib\ovation.jar %basedir%\build\cluster\modules\ext )
 
+mkdir %basedir%\Browser\release\modules\ext
 copy /Y %basedir%\lib\ovation.jar %basedir%\Browser\release\modules\ext
+
+mkdir %basedir%\DBConnectionProvider\release\modules\ext
 copy /Y %basedir%\lib\ovation.jar %basedir%\DBConnectionProvider\release\modules\ext
+
+mkdir %basedir%\OvationAPI\release\modules\ext
 copy /Y %basedir%\lib\ovation.jar %basedir%\OvationAPI\release\modules\ext
 
+mkdir %basedir%\TestManager\release\modules\ext
 copy /Y %basedir%\lib\test-manager.jar %basedir%\TestManager\release\modules\ext
 
+mkdir %basedir%\GlobalInterfaceLibrary\release\modules\ext\
 copy /Y %basedir%\lib\interfaces.jar %basedir%\GlobalInterfaceLibrary\release\modules\ext\

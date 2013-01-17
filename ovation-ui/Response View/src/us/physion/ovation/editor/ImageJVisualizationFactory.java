45836994452876c6aaa5931c75b60f184666ce6b
Barry Wark <barrywark@gmail.com>
2013-01-17 01:35:48 -0500

    Starting ImageJVisualization

--- ovation-ui/Response View/src/us/physion/ovation/editor/ImageJVisualizationFactory.java
+++ ovation-ui/Response View/src/us/physion/ovation/editor/ImageJVisualizationFactory.java
@@ -6,6 +6,7 @@ package us.physion.ovation.editor;
 
 import imagej.ImageJ;
 import imagej.display.DisplayService;
+import java.io.File;
 import net.imglib2.img.ImgPlus;
 import net.imglib2.io.ImgIOException;
 import net.imglib2.io.ImgOpener;
@@ -24,27 +25,14 @@ public class ImageJVisualizationFactory implements VisualizationFactory{
 
     @Override
     public Visualization createVisualization(Response r) {
-        /*if (r instanceof URLResponse)
-        {
-            try {
-                ImgPlus ip = ImgOpener.open(((URLResponse)r).getURLString());
-		// display the dataset
-                DisplayService displayService = new ImageJ().getService(DisplayService.class);
-                displayService.getActiveDisplay().display(ip);
-                return new ImageJVisualization(((URLResponse)r).getURLString());
-            } catch (ImgIOException ex) {
-                throw new OvationException("Unable to open image " + ex.getMessage());
-            }
-        }*/
-        
-        return new ImageJVisualization(null);
+        return new ImageJVisualization(r);
     }
 
     @Override
     public int getPreferenceForDataContainer(Response r) {
         if (r.getUTI().toLowerCase().contains("tif"))
         {
-            return 180;
+            return 110;
         }
         return -1;
     }
